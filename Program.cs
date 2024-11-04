c42dc3e03364060b395fb11af028c58537d02050
Miguel <miguelsosa2003@gmail.com>
2024-11-04 09:32:01 -0500

    Add Swagger UI configuration for development environment

--- Program.cs
+++ Program.cs
@@ -14,6 +14,18 @@ builder.Services.AddOpenApiDocument(config =>
 
 var app = builder.Build();
 
+if (app.Environment.IsDevelopment())
+{
+    app.UseOpenApi();
+    app.UseSwaggerUi(config =>
+    {
+        config.DocumentTitle = "TodoAPI";
+        config.Path = "/swagger";
+        config.DocumentPath = "/swagger/{documentName}/swagger.json";
+        config.DocExpansion = "list";
+    });
+}
+
 app.MapGet("/todoitems", async (TodoDb db) =>
     await db.Todos.ToListAsync());
 
